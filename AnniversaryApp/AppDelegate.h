5f93e281e6b611e78d5ea081ecc19454124bc0d1
HeQichang <heqichang@HeQichangdeMacBook-Pro.local>
2014-07-02 15:31:57 +0800

    Initial Commit

---
+++ AnniversaryApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnniversaryApp
+//
+//  Created by HeQichang on 14-7-2.
+//  Copyright (c) 2014年 heqichang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnniversaryAppTests/AnniversaryAppTests.m
@@ -0,0 +1,34 @@
+//
+//  AnniversaryAppTests.m
+//  AnniversaryAppTests
+//
+//  Created by HeQichang on 14-7-2.
+//  Copyright (c) 2014年 heqichang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnniversaryAppTests : XCTestCase
+
+@end
+
+@implementation AnniversaryAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
