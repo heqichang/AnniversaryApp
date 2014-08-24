6873c8fdcb034a2fcd241dc7a253f758632d8b39
HeQichang <heqichang@HeQichangdeMacBook-Pro.local>
2014-08-24 17:15:09 +0800

    迁入RecordDAO

---
+++ AnniversaryApp/RecordDAO.h
@@ -0,0 +1,13 @@
+//
+//  RecordDao.h
+//  AnniversaryApp
+//
+//  Created by HeQichang on 14-8-22.
+//  Copyright (c) 2014年 heqichang. All rights reserved.
+//
+
+#import "CoreDAO.h"
+
+@interface RecordDao : CoreDAO
+
+@end
---
+++ AnniversaryApp/RecordDAO.m
@@ -0,0 +1,13 @@
+//
+//  RecordDao.m
+//  AnniversaryApp
+//
+//  Created by HeQichang on 14-8-22.
+//  Copyright (c) 2014年 heqichang. All rights reserved.
+//
+
+#import "RecordDao.h"
+
+@implementation RecordDao
+
+@end
